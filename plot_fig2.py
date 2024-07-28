e2e9348cc19d8114b05d04a128046644044e6f8c
Robyn Stuart <robyna.s@gmail.com>
2024-07-29 08:29:24 +1000

    tidy: remove costs and s&t

--- plot_fig2.py
+++ plot_fig2.py
@@ -21,37 +21,21 @@ def preprocess_data(msim_dict, cost_dict=None):
         base_label = f'Adolescent: {np.round(cov_val, decimals=1)} coverage'
         si = sc.findinds(msim_dict[base_label].year, start_year)[0]
         di = sc.findinds(msim_dict[base_label].daly_years, start_year)[0]
-        base_dalys = msim_dict[base_label].dalys[di:]
+        # base_dalys = msim_dict[base_label].dalys[di:]
 
-    for en, eff_val in enumerate(efficacy_arr):
-        cov_val = eff_val*0.9/0.95
-        base_label = f'Adolescent: {np.round(cov_val, decimals=1)} coverage'
-        scen_label = f'Infants: {np.round(eff_val, decimals=3)} efficacy'
-        scen_dalys = msim_dict[scen_label].dalys[di:]
-        dalys_averted = sum(base_dalys - scen_dalys)
-        records += {'coverage': int(round(cov_val, 1)*100), 'efficacy': int(round(eff_val, 1)*100), 'metric':'DALYs', 'val': dalys_averted}
+        for en, eff_val in enumerate(efficacy_arr):
+            # cov_val = eff_val*0.9/0.95
+            base_label = f'Adolescent: {np.round(cov_val, decimals=1)} coverage'
+            scen_label = f'Infants: {np.round(eff_val, decimals=3)} efficacy'
+            # scen_dalys = msim_dict[scen_label].dalys[di:]
+            # dalys_averted = sum(base_dalys - scen_dalys)
+            # records += {'coverage': int(round(cov_val, 1)*100), 'efficacy': int(round(eff_val, 1)*100), 'metric':'DALYs', 'val': dalys_averted}
 
-        for pn, metric in enumerate(metrics):
-            base_vals = msim_dict[base_label][metric].values[si:]
-            scen_vals = msim_dict[scen_label][metric].values[si:]
-            n_averted = sum(base_vals - scen_vals)
-            records += {'coverage': int(round(cov_val, 1)*100), 'efficacy': int(round(eff_val, 1)*100), 'metric': f'{metric.replace("_"," ").capitalize()}', 'val': n_averted}
-
-        # Costs
-        if cost_dict is not None:
-            scen_costs = 0
-            base_costs = 0
-            for cname, cost in cost_dict.items():
-                if msim_dict[scen_label].get(cname):
-                    scen_costs += msim_dict[scen_label][cname].values * cost
-                if msim_dict[base_label].get(cname):
-                    base_costs += msim_dict[scen_label][cname].values * cost
-
-            total_scen_cost = sum([i / 1.03 ** t for t, i in enumerate(scen_costs)])
-            total_base_cost = sum([i / 1.03 ** t for t, i in enumerate(base_costs)])
-            additional_costs = total_scen_cost - total_base_cost
-
-            records += {'coverage': int(round(cov_val, 1)*100), 'efficacy': int(round(eff_val, 1)*100), 'metric': 'cost', 'val': additional_costs}
+            for pn, metric in enumerate(metrics):
+                base_vals = msim_dict[base_label][metric].values[si:]
+                scen_vals = msim_dict[scen_label][metric].values[si:]
+                n_averted = sum(base_vals - scen_vals)
+                records += {'coverage': int(round(cov_val, 1)*100), 'efficacy': int(round(eff_val, 1)*100), 'metric': f'{metric.replace("_"," ").capitalize()}', 'val': n_averted}
 
     df = pd.DataFrame.from_dict(records)
 
@@ -73,12 +57,12 @@ def plot_fig2(df):
         sharey=False,
         height=5, aspect=3,
     )
-    g.set_axis_labels("Vaccine efficacy for infants", "")
+    g.set_axis_labels("Vaccine efficacy for infants (%)", "")
     g.set_titles("{row_name} averted")
 
     for ax in g.axes.flat:
         sc.SIticks(ax)
-    g.legend.set_title("Adolescent\ncoverage")
+    g.legend.set_title("Adolescent\ncoverage (%)")
 
     # fig.tight_layout()
     fig_name = 'figures/fig2_vx_impact.png'
@@ -90,7 +74,7 @@ def plot_fig2(df):
 if __name__ == '__main__':
 
     # Load scenarios and construct figure
-    msim_dict = sc.loadobj('results/vx_scens.obj')
+    msim_dict = sc.loadobj('results/vx_scens_all.obj')
     df = preprocess_data(msim_dict)
 
     plot_fig2(df)
--- run_scenarios.py
+++ run_scenarios.py
@@ -32,72 +32,11 @@ efficacy_dict = dict(
     all=np.arange(.5, 1, .1),
     equiv=0.95*coverage_arr/.9
 )
-efficacy_scen = 'all'
+efficacy_scen = 'equiv'
 efficacy_arr = efficacy_dict[efficacy_scen]
 
 
 # %% Create interventions
-
-def make_st(screen_coverage=0.15, treat_coverage=0.7, start_year=2020):
-    """ Make screening & treatment intervention """
-
-    age_range = [30, 50]
-    len_age_range = (age_range[1]-age_range[0])/2
-    model_annual_screen_prob = 1 - (1 - screen_coverage)**(1/len_age_range)
-
-    screen_eligible = lambda sim: np.isnan(sim.people.date_screened) | \
-                                  (sim.t > (sim.people.date_screened + 5 / sim['dt']))
-    screening = hpv.routine_screening(
-        prob=model_annual_screen_prob,
-        eligibility=screen_eligible,
-        start_year=start_year,
-        product='hpv',
-        age_range=age_range,
-        label='screening'
-    )
-
-    # Assign treatment
-    screen_positive = lambda sim: sim.get_intervention('screening').outcomes['positive']
-    assign_treatment = hpv.routine_triage(
-        start_year=start_year,
-        prob=1.0,
-        annual_prob=False,
-        product='tx_assigner',
-        eligibility=screen_positive,
-        label='tx assigner'
-    )
-
-    ablation_eligible = lambda sim: sim.get_intervention('tx assigner').outcomes['ablation']
-    ablation = hpv.treat_num(
-        prob=treat_coverage,
-        annual_prob=False,
-        product='ablation',
-        eligibility=ablation_eligible,
-        label='ablation'
-    )
-
-    excision_eligible = lambda sim: list(set(sim.get_intervention('tx assigner').outcomes['excision'].tolist() +
-                                             sim.get_intervention('ablation').outcomes['unsuccessful'].tolist()))
-    excision = hpv.treat_num(
-        prob=treat_coverage,
-        annual_prob=False,
-        product='excision',
-        eligibility=excision_eligible,
-        label='excision'
-    )
-
-    radiation_eligible = lambda sim: sim.get_intervention('tx assigner').outcomes['radiation']
-    radiation = hpv.treat_num(
-        prob=treat_coverage/4,  # assume an additional dropoff in CaTx coverage
-        annual_prob=False,
-        product=hpv.radiation(),
-        eligibility=radiation_eligible,
-        label='radiation'
-    )
-
-    st_intvs = [screening, assign_treatment, ablation, excision, radiation]
-
-    return st_intvs
 
 
 def make_vx_scenarios(coverage_arr, efficacy_arr, product='nonavalent', start_year=2025):
