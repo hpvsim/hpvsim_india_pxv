ea3050c69e1943adfd6dcad63fe983a57b4ad8ee
Robyn Stuart <robyna.s@gmail.com>
2024-11-25 13:35:46 +1100

    redo age ranges

--- run_scenarios.py
+++ run_scenarios.py
@@ -27,92 +27,49 @@ import run_sim as rs
 # Settings - used here and imported elsewhere
 debug = 0
 n_seeds = [20, 1][debug]  # How many seeds to run per cluster
-coverage_arr = np.arange(.1, 1, .1)  # np.array([0.1, 0.5, 0.9])
-efficacy_dict = dict(
-    all=np.arange(.5, 1, .1),
-    equiv=0.95*coverage_arr/.9
-)
-efficacy_scen = 'equiv'
-efficacy_arr = efficacy_dict[efficacy_scen]
+target_age_list = [[9, 14], [13, 18]]
+efficacy = 0.98
+coverage_array = np.linspace(0.1, 0.9, 9)
 
 
 # %% Create interventions
+def make_vx_scenarios(target_age_list, efficacy=efficacy, product='bivalent', start_year=2025):
 
-
-def make_vx_scenarios(coverage_arr, efficacy_arr, product='nonavalent', start_year=2025):
-
-    age_range = (9, 14)
-    catchup_age = (age_range[0]+1, age_range[1])
-    routine_age = (age_range[0], age_range[0]+1)
     prod = hpv.default_vx(prod_name=product)
-    prod.imm_init = dict(dist='beta_mean', par1=0.95, par2=0.025)
+    prod.imm_init = dict(dist='uniform', par1=efficacy-0.01, par2=efficacy+0.01)
     eligibility = lambda sim: (sim.people.doses == 0)
 
     vx_scenarios = dict()
 
-    # Baseline
-    vx_scenarios['Baseline'] = []
+    # # Baseline
+    # vx_scenarios['Baseline'] = []
 
     # Construct the adolescent only scenarios
-    for cov_val in coverage_arr:
-        label = f'Adolescent: {np.round(cov_val, decimals=2)} coverage'
-        routine_vx = hpv.routine_vx(
-            prob=cov_val,
-            start_year=start_year,
-            product=prod,
-            age_range=routine_age,
-            eligibility=eligibility,
-            label='Routine vx'
-        )
+    for age_range in target_age_list:
+        catchup_age = (age_range[0]+1, age_range[1])
+        routine_age = (age_range[0], age_range[0]+1)
 
-        catchup_vx = hpv.campaign_vx(
-            prob=cov_val,
-            years=start_year,
-            product=prod,
-            age_range=catchup_age,
-            eligibility=eligibility,
-            label='Catchup vx'
-        )
+        for cov_val in coverage_array:
+            label = f'Girls {age_range[0]}-{age_range[1]}: {np.round(cov_val, decimals=2)} coverage'
+            routine_vx = hpv.routine_vx(
+                prob=cov_val,
+                start_year=start_year,
+                product=prod,
+                age_range=routine_age,
+                eligibility=eligibility,
+                label='Routine vx'
+            )
 
-        vx_scenarios[label] = [routine_vx, catchup_vx]
+            catchup_vx = hpv.campaign_vx(
+                prob=cov_val,
+                years=start_year,
+                product=prod,
+                age_range=catchup_age,
+                eligibility=eligibility,
+                label='Catchup vx'
+            )
 
-    # Construct the infant scenarios
-    for eff_val in efficacy_arr:
-
-        cov_val = eff_val*0.9/0.95
-        label = f'Infants: {np.round(eff_val, decimals=3)} efficacy'
-
-        routine_vx = hpv.routine_vx(
-            prob=cov_val,
-            years=[start_year, start_year+9],
-            product=prod,
-            age_range=routine_age,
-            eligibility=eligibility,
-            label='Routine vx'
-        )
-
-        catchup_vx = hpv.campaign_vx(
-            prob=cov_val,  #[0.9, 0.7],
-            years=start_year,  #[2025, 2030],
-            product=prod,
-            age_range=catchup_age,
-            eligibility=eligibility,
-            label='Catchup vx'
-        )
-
-        infant_prod = hpv.default_vx(prod_name=product)
-        infant_prod.imm_init = dict(dist='beta_mean', par1=eff_val, par2=0.025)
-        infant_vx = hpv.routine_vx(
-            prob=0.9,
-            start_year=start_year,
-            product=infant_prod,
-            age_range=(0, 1),
-            eligibility=eligibility,
-            label='Infant vx'
-        )
-
-        these_intvs = [infant_vx, routine_vx, catchup_vx]  #[routine_vx, catchup_vx, infant_vx]
-        vx_scenarios[label] = these_intvs
+            vx_scenarios[label] = [routine_vx, catchup_vx]
 
     return vx_scenarios
 
@@ -155,7 +112,7 @@ if __name__ == '__main__':
     # Run scenarios (usually on VMs, runs n_seeds in parallel over M scenarios)
     if do_run:
         calib_pars = sc.loadobj('results/india_pars.obj')
-        vx_scenarios = make_vx_scenarios(coverage_arr, efficacy_arr)
+        vx_scenarios = make_vx_scenarios(target_age_list=target_age_list)
         msim = run_sims(vx_scenarios=vx_scenarios)
 
         if do_save: msim.save('results/vs.msim')
@@ -165,7 +122,7 @@ if __name__ == '__main__':
             metrics = ['year', 'asr_cancer_incidence', 'n_precin_by_age', 'n_females_alive_by_age', 'cancers', 'cancer_deaths']
 
             # Process results
-            vx_scenarios = make_vx_scenarios(coverage_arr, efficacy_arr)
+            vx_scenarios = make_vx_scenarios(target_age_list=target_age_list)
             scen_labels = list(vx_scenarios.keys())
             mlist = msim.split(chunks=len(scen_labels))
 
@@ -173,15 +130,8 @@ if __name__ == '__main__':
             for si, scen_label in enumerate(scen_labels):
                 reduced_sim = mlist[si].reduce(output=True)
                 mres = sc.objdict({metric: reduced_sim.results[metric] for metric in metrics})
-                mres['dalys'] = reduced_sim.get_analyzer().dalys
-                mres['daly_years'] = reduced_sim.get_analyzer().years
-
-                for ii, intv in enumerate(reduced_sim['interventions']):
-                    intv_label = intv.label
-                    mres[intv_label] = reduced_sim['interventions'][ii].n_products_used
-
                 msim_dict[scen_label] = mres
 
-            sc.saveobj(f'results/vx_scens_{efficacy_scen}.obj', msim_dict)
+            sc.saveobj(f'results/vx_scens.obj', msim_dict)
 
     print('Done.')
